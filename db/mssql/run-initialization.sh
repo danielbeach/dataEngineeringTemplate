0a6397254f80845e44acb3538a9b95b57454876f
Tom Juntunen <tjunts@me.com>
2022-05-10 23:01:36 -0700

    adding psql and mssql services, updated readme

---
+++ db/mssql/run-initialization.sh
@@ -0,0 +1,6 @@
+# Wait to be sure that SQL Server came up
+sleep 60s
+
+# Run the setup script to create the DB and the schema in the DB
+# Note: make sure that your password matches what is in the Dockerfile
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i sql/create-database.sql
