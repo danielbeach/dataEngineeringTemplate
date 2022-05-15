5028a315dde38f8a8dd0352471b57590e955a143
Tom Juntunen <43662466+tom-juntunen@users.noreply.github.com>
2022-05-15 15:13:40 -0700

    Update run-initialization.sh

--- db/mssql/run-initialization.sh
+++ db/mssql/run-initialization.sh
@@ -3,4 +3,4 @@ sleep 60s
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i sql/create-database.sql
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $MSSQL_SA_PASSWORD -d master -i build/create-database.sql
